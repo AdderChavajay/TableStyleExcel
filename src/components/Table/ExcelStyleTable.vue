fad69b50d3dea99595d7419c5efa690fa991ce33
bohemio <adderchavajay7@gmail.com>
2022-11-29 23:35:13 -0600

    style table is done

--- src/components/Table/ExcelStyleTable.vue
+++ src/components/Table/ExcelStyleTable.vue
@@ -1,9 +1,19 @@
+<script setup></script>
+
 <template>
   <div class="w-full flex h-screen justify-center py-4">
-    <div>
-      <div>
-        <input type="text" class="border" />
-      </div>
+    <div class="w-aut">
+      <table class="">
+        <thead></thead>
+        <tbody>
+          <td class="border border-slate-600">
+            <input
+              type="text"
+              class="h-auto border-none focus:outline-none text-sm font-semibold px-1"
+            />
+          </td>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
