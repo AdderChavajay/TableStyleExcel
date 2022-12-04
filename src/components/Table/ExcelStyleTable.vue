fb8d209a4089ba824d26b269d7a3fbb8ad6c1154
bohemio <adderchavajay7@gmail.com>
2022-12-03 23:08:58 -0600

    multiply table is done and style is done

--- src/components/Table/ExcelStyleTable.vue
+++ src/components/Table/ExcelStyleTable.vue
@@ -1,17 +1,50 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const rows = ref([1]);
+const columns = ref([1]);
+const inputValue = "";
+
+const addRow = () => {
+  rows.value.push(inputValue.value);
+};
+
+const addColumn = () => {
+  columns.value.push(inputValue.value);
+};
+</script>
 
 <template>
-  <div class="w-full flex h-screen justify-center py-4">
-    <div class="w-aut">
+  <div class="w-full flex-col flex justify-center items-center space-y-3 py-4">
+    <div class="flex justify-center space-x-6">
+      <button
+        @click="addRow"
+        class="px-2 py-2 text-sm font-semibold rounded-full bg-sky-400"
+      >
+        +1 row
+      </button>
+      <button
+        @click="addColumn"
+        class="px-2 py-2 text-sm font-semibold rounded-full bg-sky-400"
+      >
+        +1 column
+      </button>
+    </div>
+    <div
+      class="flex flex-col space-y-2 max-w-7xl max-h-[80vh] overflow-x-auto overscroll-x-contain p-3"
+    >
       <table class="">
         <thead></thead>
         <tbody>
-          <td class="border border-slate-600">
-            <input
-              type="text"
-              class="h-auto border-none focus:outline-none text-sm font-semibold px-1"
-            />
-          </td>
+          <tr class="" v-for="row in rows">
+            <td class="border border-slate-600" v-for="column in columns">
+              <input
+                type="text"
+                class="h-auto border-none focus:outline-none text-sm font-semibold px-1"
+                v-model="inputValue"
+              />
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
